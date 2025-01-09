3aa2ba33b0c54642ff5302d04afb203d2b950f46
정상미 <w2321@e-mirim.hs.kr>
2025-01-09 21:05:30 +0900

    ✨ [feat] : React 로 홈페이지 만들기 - 스크롤바 컴포넌트 구성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import MainImage from "./components/MainImage/MainImage";
+import BorderBottom from "./components/BorderBottom/BorderBottom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="borderbottom"></div>
+      <div className="Bigborderbottom"></div>
       <MainImage />
+      <BorderBottom />
     </div>
   );
 }
